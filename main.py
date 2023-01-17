b09bef031215372c2b4c8c205e58a5bf63240dcf
TimShaw1 <timothyshaw0225@gmail.com>
2023-01-17 16:28:12 -0500

    Added print constant task positions

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from utility import *
 
 def printConstantGameData():
     try:
+        load_dict()
         while True:
             data = getGameData()
             if not data:
                 continue
             for key in data.keys():
                 print(data[key])
+            for i in range(len(data["tasks"])):
+                print(f"{data['tasks'][i]}: ", end='')
+                print(get_task_position(data, i))
             print()
             time.sleep(2)
 
@@ -29,8 +33,24 @@ def printGameData():
         print(get_task_position(data, i))
     print()
 
+def printConstantTaskPositions():
+    try:
+        load_dict()
+        while True:
+            data = getGameData()
+            if not data:
+                continue
+            for i in range(len(data["tasks"])):
+                print(f"{data['tasks'][i]}: ", end='')
+                print(get_task_position(data, i))
+            print()
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        pass
+
 
 if __name__ == "__main__":
-    printGameData()
+    printConstantTaskPositions()
 
 
