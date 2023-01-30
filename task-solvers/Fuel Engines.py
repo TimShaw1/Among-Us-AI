92b700d328812fe3f8134139dcdc864b563e6523
TimShaw1 <timothyshaw0225@gmail.com>
2023-01-29 20:29:15 -0500

    fuel engines done

---
+++ task-solvers/Fuel Engines.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+from task_utility import *
+import time
+import cv2
+import copy
+
+click_use()
+time.sleep(0.8)
+
+dimensions = get_dimensions()
+
+x = dimensions[0] + round(dimensions[2] / 1.31)
+y = dimensions[1] + round(dimensions[3] / 1.24)
+
+pyautogui.moveTo((x,y))
+pyautogui.dragTo(x,y, duration=2.8)
