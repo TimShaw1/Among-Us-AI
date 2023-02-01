dab8f6658a9e32a6a8fcfb53af329b231b275a6a
TimShaw1 <timothyshaw0225@gmail.com>
2023-01-31 20:56:25 -0500

    Unlock manifolds done

---
+++ task-solvers/Unlock Manifolds.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+from task_utility import *
+import time
+import pyautogui
+
+click_use()
+time.sleep(0.8)
+
+dimensions = get_dimensions()
+dimensions[0] += round(dimensions[2] / 3.4)
+dimensions[1] += round(dimensions[3] / 2.9)
+dimensions[2] = round(dimensions[2] / 2.4)
+dimensions[3] = round(dimensions[3] / 3.1)
+
+pos_list = []
+
+for i in range(1, 11):
+    pos = pyautogui.locateCenterOnScreen(f"{get_dir()}\\task-solvers\\cv2-templates\\Unlock Manifolds\\{i}.png", confidence=0.8, region=dimensions, grayscale=True)
+    pos_list.append(pos)
+
+for pos in pos_list:
+    pyautogui.click(pos)
