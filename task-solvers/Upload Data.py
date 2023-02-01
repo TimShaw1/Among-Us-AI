3ce8df3a3d72778d686bb2bd1ae1529101a12f55
TimShaw1 <timothyshaw0225@gmail.com>
2023-01-31 21:07:45 -0500

    Upload Data done

---
+++ task-solvers/Upload Data.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+from task_utility import *
+import time
+import cv2
+import copy
+import pyautogui
+
+click_use()
+time.sleep(0.8)
+
+dimensions = get_dimensions()
+
+x = dimensions[0] + round(dimensions[2] / 2.01)
+y = dimensions[1] + round(dimensions[3] / 1.64)
+pyautogui.click(x,y)
+
+time.sleep(8.1)
