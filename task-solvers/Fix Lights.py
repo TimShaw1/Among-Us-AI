7bdbf349de0072c2f3514dabb1e03b10c3c77a4c
TimShaw1 <timothyshaw0225@gmail.com>
2023-03-04 23:18:28 -0500

    fix lights done

---
+++ task-solvers/Fix Lights.py
@@ -0,0 +1,23 @@
+import time
+from task_utility import *
+import pyautogui
+import copy
+
+click_use()
+time.sleep(0.8)
+
+dimensions = get_dimensions()
+
+x = dimensions[0] + round(dimensions[2] / 3.12)
+y = dimensions[1] + round(dimensions[3] / 1.2)
+offset = round(dimensions[2] / 11.03)
+
+screenshot = get_screenshot(dimensions)
+
+print(x,y,offset, dimensions)
+
+for i in range(5):
+    color = screenshot.getpixel((x + i * offset - dimensions[0], y - dimensions[1]))
+    if color[1] < 100:
+        pyautogui.click(x + i * offset, y - round(dimensions[3] / 7.5))
+        time.sleep(0.5)
