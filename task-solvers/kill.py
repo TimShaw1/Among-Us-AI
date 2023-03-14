e0ee32be7de40564b0bdbc645eca9c0b72363ea5
TimShaw1 <timothyshaw0225@gmail.com>
2023-03-14 15:05:15 -0400

    implemented reporting and killing

---
+++ task-solvers/kill.py
@@ -0,0 +1,21 @@
+from task_utility import *
+import copy
+import pyautogui
+import time
+
+def get_kill_button_pos() -> tuple:
+    dimensions = get_dimensions()
+
+    x = dimensions[0] + round(dimensions[2] / 1.08)
+    y = dimensions[1] + round(dimensions[3] / 1.49)
+    return (x,y)
+
+def can_kill() -> bool:
+    x,y = get_kill_button_pos()
+    col = pyautogui.pixel(x, y)
+    return col[0] > 200 and col[1] > 200 and col[2] > 200
+
+def kill() -> None:
+    if not can_kill():
+        return
+    pyautogui.click(get_kill_button_pos())
---
+++ task-solvers/report.py
@@ -0,0 +1,21 @@
+from task_utility import *
+import copy
+import pyautogui
+import time
+
+def get_report_button_pos() -> tuple:
+    dimensions = get_dimensions()
+
+    x = dimensions[0] + round(dimensions[2] / 1.19)
+    y = dimensions[1] + round(dimensions[3] / 1.17)
+    return (x,y)
+
+def can_report() -> bool:
+    x,y = get_report_button_pos()
+    col = pyautogui.pixel(x, y)
+    return col[0] > 200 and col[2] < 5
+
+def report() -> None:
+    if not can_report():
+        return
+    pyautogui.click(get_report_button_pos())
