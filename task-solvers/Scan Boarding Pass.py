807420af29ab0da5578edcde43809ed25bb9ef2c
TimShaw1 <timothyshaw0225@gmail.com>
2023-06-28 13:27:40 -0400

    task done

---
+++ task-solvers/Scan Boarding Pass.py
@@ -0,0 +1,27 @@
+from task_utility import *
+import time
+import pyautogui
+
+click_use()
+time.sleep(0.3)
+
+dimensions = get_dimensions()
+
+x = dimensions[0] + round(dimensions[2] / 3.39)
+y = dimensions[1] + round(dimensions[3] / 2.08)
+
+pyautogui.click(x,y)
+time.sleep(0.3)
+
+x = dimensions[0] + round(dimensions[2] / 2.73)
+y = dimensions[1] + round(dimensions[3] / 9.23)
+
+pyautogui.click(x,y)
+time.sleep(0.3)
+
+x = dimensions[0] + round(dimensions[2] / 2.73)
+x2 = dimensions[0] + round(dimensions[2] / 1.46)
+y = dimensions[1] + round(dimensions[3] / 2)
+
+pyautogui.moveTo(x,y)
+pyautogui.dragTo(x2, y, 0.5)
