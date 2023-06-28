b001941a2d4ec601a819cc808b674f1a5024d72b
Tim Shaw <timothyshaw0225@gmail.com>
2023-06-27 20:02:44 -0400

    fix

--- task-solvers/Record Temperature.py
+++ task-solvers/Record Temperature.py
@@ -12,7 +12,7 @@ dimensions = get_dimensions()
 
 s_dimensions = copy.deepcopy(dimensions)
 s_dimensions[0] = round(s_dimensions[0] + s_dimensions[2] / 4.3)
-s_dimensions[1] = round(s_dimensions[1] + s_dimensions[2] / 2.66)
+s_dimensions[1] = round(s_dimensions[1] + s_dimensions[3] / 2.66)
 s_dimensions[2] = round(s_dimensions[2] / 5.13)
 s_dimensions[3] = round(s_dimensions[3] / 6.88)
 
@@ -20,8 +20,9 @@ x = dimensions[0] + round(dimensions[2] / 3.03)
 y_hi = dimensions[1] + round(dimensions[3] / 3.2)
 y_low = dimensions[1] + round(dimensions[3] / 1.7)
 
-screenshot = get_screenshot([x, y_hi, s_dimensions[2], s_dimensions[3]])
+screenshot = get_screenshot(s_dimensions)
 screenshot.show()
 text = pytesseract.image_to_string(screenshot.convert("L"))
+print(text)
 nums = re.findall(r'\b\d+\b', text)[0]
 print(nums)
