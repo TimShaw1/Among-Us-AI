e943a7b9ba2b786092307c048bb9124aa6ba461d
Tim Shaw <timothyshaw0225@gmail.com>
2023-07-02 10:54:01 -0400

    slow down to ensure it works

--- task-solvers/Scan Boarding Pass.py
+++ task-solvers/Scan Boarding Pass.py
@@ -11,13 +11,13 @@ x = dimensions[0] + round(dimensions[2] / 3.39)
 y = dimensions[1] + round(dimensions[3] / 2.08)
 
 pyautogui.click(x,y)
-time.sleep(0.3)
+time.sleep(0.4)
 
 x = dimensions[0] + round(dimensions[2] / 2.73)
 y = dimensions[1] + round(dimensions[3] / 9.23)
 
 pyautogui.click(x,y)
-time.sleep(0.3)
+time.sleep(0.4)
 
 x = dimensions[0] + round(dimensions[2] / 2.73)
 x2 = dimensions[0] + round(dimensions[2] / 1.46)
@@ -25,3 +25,5 @@ y = dimensions[1] + round(dimensions[3] / 2)
 
 pyautogui.moveTo(x,y)
 pyautogui.dragTo(x2, y, 0.5)
+while not is_task_done("Scan Boarding Pass"):
+    time.sleep(1/15)
