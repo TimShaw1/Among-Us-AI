9648d7c780547ce57cd04f95f9a69bb79e43b9d5
TimShaw1 <timothyshaw0225@gmail.com>
2023-07-05 21:51:40 -0400

    speed up insert keys a LOT

--- task-solvers/Insert Keys.py
+++ task-solvers/Insert Keys.py
@@ -7,22 +7,29 @@ time.sleep(0.3)
 
 dimensions = get_dimensions()
 
+#get_screen_ratio(dimensions) #2.08 10.5
+
 x = round(dimensions[0] + dimensions[2] / 2.08)
 y = round(dimensions[1] + dimensions[3] / 17.14)
 
+y2 = round(dimensions[1] + dimensions[3] / 10.5)
+
 x_offset = round(dimensions[2] / 6.64)
+y_offset = round(dimensions[3] / 5.87)
 
 if is_urgent_task():
     raise SystemExit(0)
 
+# red > 200
 for i in range(3):
-    for j in range(round(dimensions[3] / 1.2)):
-        pixel = pyautogui.pixel(x + i*x_offset, y + j)
-        if pixel[0] < 210 and pixel[0] > 204 and pixel[1] < 135 and pixel[1] > 125 and pixel[2] < 27:
-            pyautogui.moveTo(dimensions[0] + round(dimensions[2] / 4.25), dimensions[1] + round(dimensions[3] / 1.49))
-            pyautogui.dragTo(x + i*x_offset, y + j + 20, 0.7)
-            pyautogui.dragTo(x + i*x_offset + round(dimensions[2] / 9.6), y + j + round(dimensions[3] / 11), 0.3)
-            raise SystemExit(0)
+    for j in range(5):
+        for q in range(4):
+            pixel = pyautogui.pixel(x + i*x_offset, y2 + y_offset*j + q)
+            if pixel[0] > 200:
+                pyautogui.moveTo(dimensions[0] + round(dimensions[2] / 4.25), dimensions[1] + round(dimensions[3] / 1.49))
+                pyautogui.moveTo(x + i*x_offset, y2 + j*y_offset + 20, 0.7)
+                pyautogui.moveTo(x + i*x_offset + round(dimensions[2] / 9.6), y2 + j*y_offset + round(dimensions[3] / 11), 0.3)
+                raise SystemExit(0)
 
 # 206 130 24
 
