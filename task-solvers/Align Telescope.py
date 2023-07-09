c950e787c6aee4817a8c56242dcef60e0c09141d
Tim Shaw <timothyshaw0225@gmail.com>
2023-07-09 12:49:25 -0400

    fix for new resolution

--- task-solvers/Align Telescope.py
+++ task-solvers/Align Telescope.py
@@ -15,6 +15,7 @@ times = {"broken" : 0.9, "nebula" : 2, "dyson" : 2.5, "ship" : 1.4, "galaxy" : 2
 x = dim[0] + round(dim[2] / 1.35)
 y = dim[1] + round(dim[3] / 1.24)
 pixel = pyautogui.pixel(x,y)
+print(pixel)
 if is_urgent_task():
     raise SystemExit(0)
 
@@ -31,13 +32,13 @@ elif max(abs(numpy.subtract(pixel, (78, 74, 107)))) < 3:
 elif max(abs(numpy.subtract(pixel, (255, 255, 255)))) < 3:
     name = "galaxy"
 
-elif max(abs(numpy.subtract(pixel, (84, 114, 118)))) < 3:
+elif max(abs(numpy.subtract(pixel, (84, 114, 118)))) < 3 or max(abs(numpy.subtract(pixel, (76, 102, 107)))) < 3:
     name = "nebula"
 
 elif max(abs(numpy.subtract(pixel, (10, 6, 8)))) < 3:
     name = "broken"
 
-elif max(abs(numpy.subtract(pixel, (93, 133, 124)))) < 3:
+elif max(abs(numpy.subtract(pixel, (93, 133, 124)))) < 3 or max(abs(numpy.subtract(pixel, (77, 108, 102)))) < 3:
     name = "green"
 
 x_angle = cos(angles[name])
