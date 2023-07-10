afb44ab249da303f0e4c6ade3b2e14ab422c3752
Tim Shaw <timothyshaw0225@gmail.com>
2023-07-10 14:33:57 -0400

    fix key being a low one not working

--- task-solvers/Insert Keys.py
+++ task-solvers/Insert Keys.py
@@ -1,6 +1,7 @@
 from task_utility import *
 import time
 import pyautogui
+import keyboard
 
 click_use()
 time.sleep(0.3)
@@ -15,21 +16,25 @@ y = round(dimensions[1] + dimensions[3] / 17.14)
 y2 = round(dimensions[1] + dimensions[3] / 10.5)
 
 x_offset = round(dimensions[2] / 6.64)
-y_offset = round(dimensions[3] / 5.87)
+y_offset = round(dimensions[3] / 6.1)
 
 if is_urgent_task():
     raise SystemExit(0)
 
-# red > 200
-for i in range(3):
-    for j in range(5):
-        for q in range(4):
-            pixel = pyautogui.pixel(x + i*x_offset, y2 + y_offset*j + q)
-            if pixel[0] > 200:
-                pyautogui.moveTo(dimensions[0] + round(dimensions[2] / 4.25), dimensions[1] + round(dimensions[3] / 1.49))
-                pyautogui.dragTo(x + i*x_offset, y2 + j*y_offset + 20, 0.7)
-                pyautogui.dragTo(x + i*x_offset + round(dimensions[2] / 9.6), y2 + j*y_offset + round(dimensions[3] / 11), 0.3)
-                raise SystemExit(0)
+while not is_task_done("Insert Keys"):
+    # red > 200
+    for i in range(3):
+        for j in range(5):
+            for q in range(4):
+                pixel = pyautogui.pixel(x + i*x_offset, y2 + y_offset*j + q)
+                if pixel[0] > 200:
+                    pyautogui.moveTo(dimensions[0] + round(dimensions[2] / 4.25), dimensions[1] + round(dimensions[3] / 1.49))
+                    pyautogui.dragTo(x + i*x_offset, y2 + j*y_offset + 20, 0.7)
+                    pyautogui.dragTo(x + i*x_offset + round(dimensions[2] / 9.6), y2 + j*y_offset + round(dimensions[3] / 11), 0.3)
+                    raise SystemExit(0)
+                
+    if keyboard.is_pressed('`'):
+        raise SystemExit(0)
 
 # 206 130 24
 
